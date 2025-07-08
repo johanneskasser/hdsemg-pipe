91fc06ad22185b232d792e65bdc608e639151d0b
Johannes Kasser <johanneskasser@outlook.de>
2025-07-08 18:46:58 +0200

    feat: remove cpuonly variant from PyTorch installation in environment.yml, requirements.txt, and setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         "numpy>=1.21.0",
         "scipy>=1.7.0",
         "pandas>=1.3.5",
-        "torch>=1.10.0+cpu",
+        "torch>=1.10.0",
         "requests>=2.26.0",
         "hdsemg-shared>=0.11.1",
         "hdsemg-select>=0.1.1",
